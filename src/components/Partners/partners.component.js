867a4148ed21be8dc0fbd68c288478f90352ad10
Nelkisa Matias <gabrielamatias60@gmail.com>
2021-12-01 20:15:15 -0300

    Correção de layout e edição da seção de dúvidas frequentes

--- src/components/Partners/partners.component.js
+++ src/components/Partners/partners.component.js
@@ -1,50 +1,42 @@
 import styles from '../../../styles/partners.module.css'
-export default function Partners(){
-    return(
+export default function Partners() {
+    return (
         <div className={styles.parceiros}>
-        <div className="row">
-            <h3 className={styles.parceiros_title}>Clínicas Parceiras</h3>
+            <div className="row">
+                <h3 className={styles.parceiros_title}>Clínicas Parceiras</h3>
 
-            <div className={styles.partnerContent}>
-                <div >
-                    <h5>
-                        Ok Doutor
-                    </h5>
-                    <p className={styles.descricao_parceiros}>
-                        Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
-                        climatizadas, conforto e excelente atendimento.
-                    </p>
+                <div className={styles.partnerContent}>
+                    <div >
+                        <a>Ok Doutor</a>
+                        <p className={styles.descricao_parceiros}>
+                            Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
+                            climatizadas, conforto e excelente atendimento.
+                        </p>
+                    </div>
+                    <div >
+                        <a>Ok Doutor</a>
+                        <p className={styles.descricao_parceiros}>
+                            Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
+                            climatizadas, conforto e excelente atendimento.
+                        </p>
+                    </div>
+                    <div >
+                        <a>Ok Doutor</a>
+                        <p className={styles.descricao_parceiros}>
+                            Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
+                            climatizadas, conforto e excelente atendimento.
+                        </p>
+                    </div>
+                    <div >
+                        <a>Ok Doutor</a>
+                        <p className={styles.descricao_parceiros}>
+                            Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
+                            climatizadas, conforto e excelente atendimento.
+                        </p>
+                    </div>
                 </div>
-                <div >
-                    <h5>
-                        Ok Doutor
-                    </h5>
-                    <p className={styles.descricao_parceiros}>
-                        Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
-                        climatizadas, conforto e excelente atendimento.
-                    </p>
-                </div>
-                <div >
-                    <h5>
-                        Ok Doutor
-                    </h5>
-                    <p className={styles.descricao_parceiros}>
-                        Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
-                        climatizadas, conforto e excelente atendimento.
-                    </p>
-                </div>
-                <div >
-                    <h5>
-                        Ok Doutor
-                    </h5>
-                    <p className={styles.descricao_parceiros}>
-                        Unidades próximas de você, atendimento de qualidade sem precisar ir muito longe! Estruturas
-                        climatizadas, conforto e excelente atendimento.
-                    </p>
-                </div>
+
             </div>
-
         </div>
-    </div>
     )
 }
